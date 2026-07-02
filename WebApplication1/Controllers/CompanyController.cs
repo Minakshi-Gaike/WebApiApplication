888ee087a3267a17da81a466cc6927772e23fe9e
Minakshi-Gaike <minakshigaike@gmail.com>
2026-07-02 11:05:43 +0530

    added

--- WebApplication1/Controllers/CompanyController.cs
+++ WebApplication1/Controllers/CompanyController.cs
@@ -2,10 +2,21 @@
 
 namespace WebApplication1.Controllers
 {
-    [Route("api/[company]")]
+    //[Route("api/[company]")]
     [ApiController]
     public class CompanyController : ControllerBase
     {
-
-    }
+    [HttpGet]
+        [Route("api/company")]
+        public IActionResult GetCompany()
+        {
+            var company = new
+            {
+                Id = 1,
+                Name = "My Company",
+                Address = "123 Main St",
+                City = "Anytown",
+            };
+            return Ok(company);
+        }
 }
