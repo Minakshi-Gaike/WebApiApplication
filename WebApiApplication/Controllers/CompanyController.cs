e52bb9c57a41afa2b1ed79bed39c35ee40c1bd30
Minakshi-Gaike <minakshigaike@gmail.com>
2026-07-02 12:45:52 +0530

    added

---
+++ WebApiApplication/Controllers/CompanyController.cs
@@ -0,0 +1,28 @@
+﻿//using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiApplication.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/company")]
+        public IActionResult GetCompany()
+        {
+            var company = new
+            {
+                Id = 1,
+                Name = "My Company",
+                Address = "123 Main St",
+                City = "Anytown",
+            };
+            return Ok(company);
+        }
+    }
+}
+
+       
+    
+
